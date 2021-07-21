b31c5169e98ce7656a4175253b6d47a8ef4e6f6e
Jeevan <jeevan.tiwari@one97.net>
2021-07-21 13:27:24 +0530

    Initial Commit

---
+++ CreateAppleUsingMask/CreateAppleUsingMaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateAppleUsingMaskApp.swift
+//  CreateAppleUsingMask
+//
+//  Created by jeevan tiwari on 7/21/21.
+//
+
+import SwiftUI
+
+@main
+struct CreateAppleUsingMaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
