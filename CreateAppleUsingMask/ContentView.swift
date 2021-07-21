a91414a0c64d506494cee6e0da3eef3c34f4186a
Jeevan <jeevan.tiwari@one97.net>
2021-07-21 13:39:52 +0530

    Changes done

--- CreateAppleUsingMask/ContentView.swift
+++ CreateAppleUsingMask/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 0){
+            Color.green
+            Color.green
+            Color.green
+            Color.yellow
+            Color.orange
+            Color.red
+            Color.purple
+            Color.blue
+        }
+        .mask{
+            Image(systemName: "applelogo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        }
+        .frame(width: 240, height: 240)
     }
 }
 
